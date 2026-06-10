144b7d36b17be7c02476f47c59fb49715fd0f7c2
eormeno-idei <eormeno@iinfo.unsj.edu.ar>
2026-06-10 10:57:16 -0300

    refactor: remove commented-out code and logging from UIController

--- packages/idei/usim/src/Http/Controllers/UIController.php
+++ packages/idei/usim/src/Http/Controllers/UIController.php
@@ -5,7 +5,6 @@ use Idei\Usim\Screen;
 use Idei\Usim\UIChangesCollector;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class UIController extends Controller
@@ -51,40 +50,40 @@ class UIController extends Controller
 
         $allChanges = $this->uiChanges->all();
 
-        // copia allChanges y sólo deja los cambios que tengan 'type' => 'row' o 'type' => 'cell'
-        $filteredChanges = array_filter($allChanges, function ($change) {
-            $type = $change['type'] ?? null;
-            $name = $change['name'] ?? null;
-            $isTableCell = $type === 'tablecell';
-            $nameContainsAnumberBetween1And20 = true;// isset($name) && preg_match('/^users_table__(1[0-9]|20|[0-9])_/', $name);
-            $rowBetween1And20 = isset($change['row']);// && $change['row'] >= 0 && $change['row'] <= 20;
-            return $rowBetween1And20 || ($isTableCell && $nameContainsAnumberBetween1And20);
-        });
+        // // copia allChanges y sólo deja los cambios que tengan 'type' => 'row' o 'type' => 'cell'
+        // $filteredChanges = array_filter($allChanges, function ($change) {
+        //     $type = $change['type'] ?? null;
+        //     $name = $change['name'] ?? null;
+        //     $isTableCell = $type === 'tablecell';
+        //     $nameContainsAnumberBetween1And20 = true;// isset($name) && preg_match('/^users_table__(1[0-9]|20|[0-9])_/', $name);
+        //     $rowBetween1And20 = isset($change['row']);// && $change['row'] >= 0 && $change['row'] <= 20;
+        //     return $rowBetween1And20 || ($isTableCell && $nameContainsAnumberBetween1And20);
+        // });
 
-        // sanitiza los cambios filtrados para que sólo queden los campos 'type', 'name' y 'row' (si existe)
-        $filteredChanges = array_map(function ($change) {
-            $type = $change['type'] ?? null;
-            if ($type === 'tablecell') {
-                return [
-                    'column' => $change['column'] ?? null,
-                    // 'parent' => $change['parent'] ?? null,
-                    'text' => $change['text'] ?? null,
-                    // 'type' => $type,
-                    // 'name' => $change['name'] ?? null,
-                ];
-            }
-            return [
-                'row' => $change['row'] ?? null,
-                // 'parent' => $change['parent'] ?? null,
-                // 'type' => $change['type'] ?? null,
-            ];
-        }, $filteredChanges);
+        // // sanitiza los cambios filtrados para que sólo queden los campos 'type', 'name' y 'row' (si existe)
+        // $filteredChanges = array_map(function ($change) {
+        //     $type = $change['type'] ?? null;
+        //     if ($type === 'tablecell') {
+        //         return [
+        //             'column' => $change['column'] ?? null,
+        //             // 'parent' => $change['parent'] ?? null,
+        //             'text' => $change['text'] ?? null,
+        //             // 'type' => $type,
+        //             // 'name' => $change['name'] ?? null,
+        //         ];
+        //     }
+        //     return [
+        //         'row' => $change['row'] ?? null,
+        //         // 'parent' => $change['parent'] ?? null,
+        //         // 'type' => $change['type'] ?? null,
+        //     ];
+        // }, $filteredChanges);
 
 
-        Log::info("Changes: " . json_encode(
-            $filteredChanges,
-            JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
-        ));
+        // Log::info("Changes: " . json_encode(
+        //     $filteredChanges,
+        //     JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
+        // ));
 
         return response()->json($allChanges);
     }
